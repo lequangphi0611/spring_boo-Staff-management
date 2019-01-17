127434afe6133ba7c192fb9d9605418def4a2844
lequangphi0611 <lequangphi0611@gmail.com>
2019-01-17 08:58:16 +0700

    phi

---
+++ src/test/java/com/philqpd02256/Philqpd02256StaffManagementSpringBootApplicationTests.java
@@ -0,0 +1,17 @@
+package com.philqpd02256;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Philqpd02256StaffManagementSpringBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
